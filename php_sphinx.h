225bd17dbfe586859cacbf85ee1722eff43c2cd6
Antony Dovgal <tony2001@php.net>
2008-07-23 15:36:00 +0000

    it's 0.2.0-dev, btw

--- php_sphinx.h
+++ php_sphinx.h
@@ -32,7 +32,7 @@ extern zend_module_entry sphinx_module_entry;
 PHP_MINIT_FUNCTION(sphinx);
 PHP_MINFO_FUNCTION(sphinx);
 
-#define PHP_SPHINX_VERSION "0.1.0"
+#define PHP_SPHINX_VERSION "0.2.0-dev"
 
 #endif	/* PHP_SPHINX_H */
 
